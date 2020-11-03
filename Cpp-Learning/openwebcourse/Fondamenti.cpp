99803e24f68d6786e3ba6481ab75248bc68261b2
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-02 21:39:45 -0500

    Tipi di dati

--- Cpp-Learning/openwebcourse/Fondamenti.cpp
+++ Cpp-Learning/openwebcourse/Fondamenti.cpp
@@ -1,4 +1,14 @@
 /**
+=====================================
+Autore: William Martín Chávez González
+Data: 2 di novembre, 2020
+Riferimenti:
+- Openwebinars: https://openwebinars.net/academia/aprende/introduccion-c
+- C++ - sizeof: https://en.cppreference.com/w/cpp/language/sizeof
+=====================================
+*/
+
+/**
 Procedura Linker: #include <codice per la procedura Linker>
 */
 #include <iostream>
@@ -12,10 +22,50 @@ int main() {
 
     // Variabile Bool:
     bool b = true;
-    std::cout << "Variabile Boolean: " << b << " avec taglia: " << sizeof(b) << "\n" << std::endl;
+    std::cout << "Variabile Boolean: " << b << " avec sa taglia in bytes: " << sizeof(b) << "bytes \n" << std::endl;
 
-    // Character: codice ASCII: https://it.wikipedia.org/wiki/ASCII
-    char ch = 27;
+    /**
+     Character: codice ASCII: https://it.wikipedia.org/wiki/ASCII
+     - 1 byte
+     */
+    char ch = 65;
+    std::cout << "Variabile character: " << ch << " avec sa taglia in bytes: " << sizeof(ch) << "bytes \n" << std::endl;
+
+    /**
+     Wide Character:
+     - Per rapprasentare altri lingue come il cino o iul giapponese.
+     - Altri codici di characters come UNICODE.
+     - Avvertimento: occupa 2 bytes.
+     */
+     wchar_t wch_t = 'A';
+     std::cout << "Variabilie wide character: " << wch_t << " avec sa taglia in bytes: " << sizeof(wch_t) << " bytes\n"<< std::endl;
+
+     /** Interi:
+      - Sono numeri interi di:
+         * int -> 4 bytes
+         * int8_t -> 8 bytes
+         * int16_t -> 16 bytes
+         * int32_t -> 32 bytes
+         * int64_t -> 64 bytes
+     */
+     int intero = 9999;
+     std::cout << "Variabile intera: " << intero << " avec sa taglia in bytes: " << sizeof(intero) << " bytes\n" << std::endl;
+
+     /**
+        Decimali:
+        - double -> 8 bytes, doppio numeri di decimal => meglior precisione
+        - float -> 4 bytes
+          * float_denorm_style -> http://www.cplusplus.com/reference/limits/float_denorm_style/
+          * float_round_style -> http://www.cplusplus.com/reference/limits/float_round_style/
+     */
+     double doub = 90.9876224;
+     float fl = 90.9876224;
+     std::cout << "Varibile decimale double: " << doub << " avec sa taglia in bytes: " << sizeof(doub) << " bytes \n" << std::endl;
+     std::cout << "Varibile decimale float: " << fl << " avec sa taglia in bytes: " << sizeof(fl) << " bytes \n" << std::endl;
+
+     /**
+
+     */
 
     return 0;
 }
