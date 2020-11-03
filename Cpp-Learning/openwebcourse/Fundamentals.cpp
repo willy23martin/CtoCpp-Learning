5f51a843b52c83ce9da4f065d4be1ddc0d9cdf27
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-02 20:19:17 -0500

    Fundamentals of Cpp

---
+++ Cpp-Learning/openwebcourse/Fundamentals.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "WMC from hello world" <<std::endl;
+    return 0;
+}
---
+++ Cpp-Learning/src/CppBasicLearningWorkshops.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CppBasicLearningWorkshops.cpp
+// Author      : William Martín Chávez González
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World WMC!!!" << endl; // prints !!!Hello World WMC!!!
+	return 0;
+}
