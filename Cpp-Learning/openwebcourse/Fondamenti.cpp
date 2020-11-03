a96702b35844ffbe3db34ddec188ae8c8da11e6f
willy23martin <william.chavez@correo.icesi.edu.co>
2020-11-02 20:48:58 -0500

    Fondamenti di C++ in Italiano per l'apprendimento

---
+++ Cpp-Learning/openwebcourse/Fondamenti.cpp
@@ -0,0 +1,21 @@
+/**
+Procedura Linker: #include <codice per la procedura Linker>
+*/
+#include <iostream>
+
+int main() {
+
+    // Per stampare nella console:
+    std::cout << "Fondamenti di Cpp (C++) in Italiano\n" <<std::endl;
+
+    /** Tipi di Dati*/
+
+    // Variabile Bool:
+    bool b = true;
+    std::cout << "Variabile Boolean: " << b << " avec taglia: " << sizeof(b) << "\n" << std::endl;
+
+    // Character: codice ASCII: https://it.wikipedia.org/wiki/ASCII
+    char ch = 27;
+
+    return 0;
+}
--- Cpp-Learning/openwebcourse/Fundamentals.cpp
+++
@@ -1,9 +0,0 @@
-/**
-Linker process: #include <code to be linked>
-*/
-#include <iostream>
-
-int main() {
-    std::cout << "WMC from hello world" <<std::endl;
-    return 0;
-}
