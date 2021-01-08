8fc3fa9bf558eb9a5c80ca670f52baac3bfbf556
willy23martin <william.chavez@correo.icesi.edu.co>
2021-01-07 21:53:27 -0500

    aritmetica.c è stato modificato per includere calcoli di somma, sottrazione, moltiplicazione e divisione.

--- C-Learning/fundamentals/CTutorial/book-course/aritmetica.c
+++ C-Learning/fundamentals/CTutorial/book-course/aritmetica.c
@@ -5,3 +5,46 @@ Lezione di C - C/C++ Curso de programación di Francisco Javier Ceballos
   * Esercizi proposti: aritmetica.c
 */
 
+#include <stdio.h>
+#include <locale.h>
+
+int main(void){
+
+    setlocale(0, "");
+
+    double ottenere_numero();
+
+    printf("Specifici il numero uno per favore: \n");
+    double numero_uno = ottenere_numero();
+    printf("Specifici il numero due per favore: \n");
+    double numero_due = ottenere_numero();
+    printf("Specifici il numero tre per favore: \n");
+    double numero_tre = ottenere_numero();
+
+    printf("Grazie. Faremo calcoli ... \n");
+
+    double somma = numero_uno + numero_due + numero_tre;
+    printf("%lf + %lf + %lf = %lf \n", numero_uno, numero_due, numero_tre, somma);
+
+    double sottrazione = numero_uno - numero_due - numero_tre;
+    printf("%lf - %lf - %lf = %lf \n", numero_uno, numero_due, numero_tre, sottrazione);
+
+    double moltiplicazione = numero_uno * numero_due * numero_tre;
+    printf("%lf * %lf * %lf = %lf \n", numero_uno, numero_due, numero_tre, moltiplicazione);
+
+    double divisione = numero_uno / numero_due / numero_tre;
+    printf("%lf / %lf / %lf = %lf \n", numero_uno, numero_due, numero_tre, divisione);
+
+}
+
+double ottenere_numero(){
+    double risultato_numero = 0;
+    double numero;
+    while(risultato_numero == 0){
+        risultato_numero = scanf("%lf", &numero);
+        if(risultato_numero == 0) {
+            printf("Il numero deve essere corretto. \n");
+        }
+    }
+    return numero;
+}
