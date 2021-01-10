34a5671238eac7283a9af0bc18ebd3f5b4841dff
willy23martin <william.chavez@correo.icesi.edu.co>
2021-01-09 19:37:12 -0500

    C++ scope concept

---
+++ Cpp-Learning/openwebcourse/Scope.cpp
@@ -0,0 +1,30 @@
+/*
+Santiago de Cali, 9, Gennaio, 2021.
+
+Apprendimento da C a C++
+
+- Scope:  scopo delle variabili.
+
+*/
+
+#include <stdio.h>
+#include <locale>
+#include <iostream>
+
+int main(){
+
+    setlocale(LC_ALL, "Italian");
+
+    int numero = 2;
+
+    // Nuovo scopo:
+    {
+        int numero_limitato = 90;
+        numero = numero + numero_limitato; // successo dell'operazione.s
+    }
+
+    std::cout<<"Numero = " << numero << " con lunghezza: " << sizeof(numero) << " bytes." << std::endl;
+
+    // numero = numero_limitato; // Compilation error: numero_limitato was not declare in this scope.
+
+}
